622776bde9c47eeeec8c9c9a102daac2e601c89e
Isak Bengtsson <isakb97@gmail.com>
2024-02-08 15:50:39 +0100

    update loss function to a half-working condition

--- scripts/test.py
+++ scripts/test.py
@@ -22,13 +22,16 @@ def mwpm_prediction(edges, weights, classes):
     edges_w_weights = {tuple(sorted(x)): w for x, w in zip(edges.T, weights)}
     edges_w_classes = {tuple(sorted(x)): c for x, c in zip(edges.T, classes)}
     matched_edges = mwpm(edges_w_weights)
+    
+    # need to make sure matched_edges is sorted
+    matched_edges = [tuple(sorted((x[0], x[1]))) for x in matched_edges]
 
+    # REMOVE IF WHEN WE HAVE ENSURED THAT THERE IS ALWAYS AN EVEN NUMBER OF EDGES
     if matched_edges:
         classes = np.array([edges_w_classes[edge] for edge in matched_edges])
         return classes.sum() & 1
     else:
-        print("No matches")
-        return 0
+        return 1
 
 
 class MWPM:
@@ -115,11 +118,16 @@ class MWPMLoss(torch.autograd.Function):
                 pred_w = mwpm_prediction(edges, _weights, classes)
                 pred_c = mwpm_prediction(edges, weights, _classes)
                 preds_partial_de.append([pred_w, pred_c])
-            preds_grad[edge_map, :] = torch.tensor(
-                preds_partial_de, dtype=torch.float32
-            )
-            grad_help[edge_map, 0] = prediction
-            grad_help[edge_map, 1] = labels[i]
+                
+            # REMOVE WHEN WE KNOW THAT ALL SYNDROMES HAVE AN EDGE
+            if edge_map.numel() == 0:
+                continue
+            else:
+                preds_grad[edge_map, :] = torch.tensor(
+                    preds_partial_de, dtype=torch.float32
+                )
+                grad_help[edge_map, 0] = prediction
+                grad_help[edge_map, 1] = labels[i]
         preds = np.array(preds)
 
         # compute accuracy
@@ -141,7 +149,8 @@ class MWPMLoss(torch.autograd.Function):
         grad_output,
     ):
         preds, grad_help = ctx.saved_tensors
-        gradients = (grad_help[:, 0] - grad_help[:, 1]) * (preds - grad_help[:, 0])
+        print(preds.shape, grad_help.shape)
+        gradients = (grad_help[:, 0] - grad_help[:, 1])[:, None] * (preds - grad_help[:, 0][:, None])
         gradients.requires_grad = True
         return None, None, gradients, None, None, None
 
