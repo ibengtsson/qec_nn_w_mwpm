7c82263a49b016bce75bfbfbd600dd3f3056679a
Isak Bengtsson <isakb97@gmail.com>
2024-02-08 11:20:23 +0100

    add skeleton code

--- src/models.py
+++ src/models.py
@@ -1,8 +1,10 @@
+from typing import Any
 import torch
 import torch.nn as nn
 import torch_geometric.nn as nng
 from scipy.spatial.distance import cdist
 import numpy as np
+import pymatching
 
 class EdgeNet(nn.Module):
     
@@ -20,9 +22,31 @@ class EdgeNet(nn.Module):
         
         y = torch.tensor(out, requires_grad=True, dtype=torch.float32)
         return y
-
-
     
+class MWPMLoss(torch.autograd.Function):
+    
+    @staticmethod
+    def forward(ctx, syndrome, label, decoder: pymatching.Matching):
+        
+        # possible reshape and move to numpy
+        # ...
+        
+        
+        
+        ctx.save_for_backward(syndrome, label)
+        
+        
+        
+
+class SimpleTest(nn.Module):
+    
+    def __init__(self):
+        super().__init__()
+        
+    def forward(self, x):
+        pass
+    
+
         
         
         
