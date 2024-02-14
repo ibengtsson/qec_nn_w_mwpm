1eb7e7680dfa16694136095ee0e5a132eb36dacc
fridafjelddahl <fridafj@student.chalmers.se>
2024-02-14 14:20:19 +0100

    add virtual node after knn

--- src/graph.py
+++ src/graph.py
@@ -4,6 +4,7 @@ import numpy as np
 import torch
 from torch_geometric.nn import knn_graph
 from torch_geometric.transforms import Distance
+from torch_geometric.utils import unbatch
 
 
 def get_node_list_3D(syndrome_3D):
@@ -154,7 +155,6 @@ def get_batch_of_graphs(
     device=torch.device("cpu"),
 ):
     syndromes = syndromes.astype(np.float32)
-    
     defect_inds = np.nonzero(syndromes)
     defects = syndromes[defect_inds]
     
@@ -168,38 +168,27 @@ def get_batch_of_graphs(
     node_features[x_defects, 2:] = defect_inds[x_defects, ...]
     node_features[z_defects, 1] = 1
     node_features[z_defects, 2:] = defect_inds[z_defects, ...]
-
     node_features.max(axis=0)
     x_cols = [0, 1, 3, 4, 5]
     batch_col = 2
 
     # x: (X-detector, Z-detector, N-dist, W-dist, time-dist)
     x = torch.tensor(node_features[:, x_cols])
-    
     # add virtual nodes (FOR NOW: ADD VIRTUAL NODE IN THE MIDDLE OF THE GRAPH)
     label = {"z": 3, "x": 1}
     even_odd = np.count_nonzero(syndromes == label[experiment], axis=(1, 2, 3)) & 1
-
-    label = {"z": 1, "x": 0}
-    virtual_nodes = torch.zeros((np.sum(even_odd), n_node_features), dtype=torch.float32)
-    virtual_nodes[:, label[experiment]] = 1
-    virtual_nodes[:, 2:4] = (code_distance + 1) / 2
-    virtual_nodes[:, -1] = syndromes.shape[-1] / 2
-    
-    x = torch.cat((x, virtual_nodes), axis=0).to(device)
-    
+    label = {"z": 1, "x": 0}    
     batch_labels = torch.tensor(node_features[:, batch_col]).long()
-    virtual_node_labels = (torch.arange(0, syndromes.shape[0])[even_odd.astype(bool)]).long()
-    batch_labels = torch.cat((batch_labels, virtual_node_labels), axis=0).to(device)
     
     # not sure if batch labels needs to be sorted (maybe remove if slow)
-    batch_labels, sort_indx = torch.sort(batch_labels)
-    x = x[sort_indx, :]
+    #batch_labels, sort_indx = torch.sort(batch_labels)
+    #x = x[sort_indx, :]
 
     pos = x[:, 2:]
     # get edge indices 
     if m_nearest_nodes:
         edge_index = knn_graph(pos, m_nearest_nodes, batch=batch_labels)
+
     else:
         n = x.shape[0]
         edge_index = torch.cat(
@@ -223,7 +212,37 @@ def get_batch_of_graphs(
     # cast eq_class to float32
     eq_class = eq_class.type(dtype=torch.float32)
     edge_attr = torch.stack((dist**power, eq_class), dim=1)
+
+    # adding virtual nodes and edges to each graph with an odd number of nodes
+    virtual_nodes = torch.zeros((np.sum(even_odd), n_node_features), dtype=torch.float32)
+    virtual_nodes[:, label[experiment]] = 1
+    virtual_nodes[:,2:4] = -1
+    virtual_node_labels = (torch.arange(0, syndromes.shape[0])[even_odd.astype(bool)]).long()
+    node_indices = torch.arange(0,x.shape[0])
+    nodes_per_graph = list(unbatch(node_indices,batch_labels))
     
+    connect_nodes = [nodes_per_graph[i] for i in virtual_node_labels]
+    list_nodes = [x.tolist() for x in connect_nodes]
+    virtual_indices = torch.arange(x.shape[0],x.shape[0]+len(virtual_node_labels))
+
+    num_new_edges = len(sum(list_nodes,[]))*2
+    virtual_edges = torch.zeros(2,num_new_edges,dtype=int)
+    count = 0
+    for i in range(0,len(virtual_indices)):
+        curr_virtual = virtual_indices[i]
+        curr_real = connect_nodes[i]
+        for j in range(0,len(curr_real)):
+            virtual_edges[0,count] = curr_virtual
+            virtual_edges[1,count] = curr_real[j]
+            virtual_edges[0,count+1] = curr_real[j]
+            virtual_edges[1,count+1] = curr_virtual
+            count += 2
+                
+    #print(virtual_node_graph_nodes)
+    x = torch.cat((x, virtual_nodes), axis=0).to(device)
+    edge_index = torch.cat((edge_index, virtual_edges), axis=1).to(device)
+    virtual_attr = torch.ones((virtual_edges.shape[1],2),dtype=torch.float32)
+    edge_attr = torch.cat((edge_attr, virtual_attr), axis=0).to(device)
     # return indicator labelling nodes belonging to experiment (X or Z-nodes)
     detector_labels = x[:, label[experiment]] == 1
     
@@ -301,3 +320,10 @@ def extract_edges(edges, edge_attr, batch_labels, node_range):
         classes_per_syndrome,
         edge_indx,
     )
+
+
+
+        
+
+
+        
