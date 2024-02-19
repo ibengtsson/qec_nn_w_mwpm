59f5906d3a4726d3ed7af6180cdaf0ab021333fe
Isak Bengtsson <isakbe@chalmers.se>
2024-02-19 16:53:31 +0100

    finish training class

--- src/training.py
+++ src/training.py
@@ -6,11 +6,13 @@ import os
 import sys
 from pathlib import Path
 from datetime import datetime
+import random
+from typing import Callable
 
-# relative imports
-sys.path.append("../")
-from src.utils import parse_yaml
+
+from src.utils import parse_yaml, inference
 from src.simulations import SurfaceCodeSim
+from src.graph import get_batch_of_graphs
 
 
 class ModelTrainer:
@@ -18,8 +20,9 @@ class ModelTrainer:
     def __init__(
         self,
         model: nn.Module,
-        loss_fun,
+        loss_fun: Callable,
         config: os.PathLike = None,
+        save_model: bool = False,
     ):
 
         # load and initialise settings
@@ -28,7 +31,8 @@ class ModelTrainer:
         self.saved_model_path = Path(paths["saved_model_path"])
         self.graph_settings = graph_settings
         self.training_settings = training_settings
-        
+        self.save_model = save_model
+
         # current training status
         self.epoch = training_settings["current_epoch"]
         if training_settings["device"] == "cuda":
@@ -37,16 +41,16 @@ class ModelTrainer:
             )
         else:
             self.device = torch.device("cpu")
-            
+
         # create a dictionary saving training metrics
         training_history = {}
         training_history["epoch"] = self.epoch
-        training_history["train_accuracy"] = []
+        training_history["train_loss"] = []
         training_history["val_accuracy"] = []
         training_history["best_val_accuracy"] = -1
 
         self.training_history = training_history
-        
+
         # only keep best found weights
         self.optimal_weights = None
 
@@ -56,7 +60,7 @@ class ModelTrainer:
         self.optimizer = torch.optim.Adam(
             self.model.parameters(), lr=training_settings["lr"]
         )
-        
+
         # generate a unique name to not overwrite other models
         name = (
             "d"
@@ -67,13 +71,13 @@ class ModelTrainer:
         )
         current_datetime = datetime.now().strftime("%y%m%d-%H%M%S")
         self.save_name = name + current_datetime
-        
+
         # check if model should be loaded
         if training_settings["resume_training"]:
             self.load_trained_model()
-            
+
     def save_model_w_training_settings(self, model_name=None):
-        
+
         # make sure path exists, else create it
         self.save_dir.mkdir(parents=True, exist_ok=True)
         if model_name is not None:
@@ -82,16 +86,21 @@ class ModelTrainer:
             path = self.save_dir / (self.save_name + ".pt")
 
         # we only want to save the weights that corresponds to the best found accuracy
-        if self.training_history["val_accuracy"][-1] > self.training_history["best_val_accuracy"]:
-            self.training_history["best_val_accuracy"] = self.training_history["val_accuracy"][-1]
+        if (
+            self.training_history["val_accuracy"][-1]
+            > self.training_history["best_val_accuracy"]
+        ):
+            self.training_history["best_val_accuracy"] = self.training_history[
+                "val_accuracy"
+            ][-1]
             self.optimal_weights = self.model.state_dict()
-        
+
         attributes = {
             "training_history": self.training_history,
             "model": self.optimal_weights,
             "optimizer": self.optimizer.state_dict(),
             "graph_settings": self.graph_settings,
-            "training_settings": self.training_settings, 
+            "training_settings": self.training_settings,
         }
 
         torch.save(attributes, path)
@@ -102,7 +111,7 @@ class ModelTrainer:
 
         # update attributes and load model with trained weights
         self.training_history = saved_attributes["training_history"]
-        
+
         # older models do not have the attribute "best_val_accuracy"
         if not "best_val_accuracy" in self.training_history:
             self.training_history["best_val_accuracy"] = -1
@@ -110,10 +119,10 @@ class ModelTrainer:
         self.model.load_state_dict(saved_attributes["model"])
         self.optimizer.load_state_dict(saved_attributes["optimizer"])
         self.save_name = self.save_name + "_load_f_" + model_path.name.split(sep=".")[0]
-        
+
         # only keep best found weights
         self.optimal_weights = saved_attributes["model"]
-        
+
     def initialise_simulations(self, n=5):
         # simulation settings
         code_size = self.graph_settings["code_size"]
@@ -125,14 +134,27 @@ class ModelTrainer:
 
         error_rates = np.linspace(min_error_rate, max_error_rate, n)
 
+        task_dict = {
+            "z": "surface_code:rotated_memory_z",
+            "x": "surface_code:rotated_memory_x",
+        }
+        code_task = task_dict[self.graph_settings["experiment"]]
+
         sims = []
         for p in error_rates:
-            sim = SurfaceCodeSim(reps, code_size, p, batch_size)
+            sim = SurfaceCodeSim(
+                reps,
+                code_size,
+                p,
+                batch_size,
+                code_task=code_task,
+            )
             sims.append(sim)
 
         return sims
 
     def create_test_set(self, n_graphs=5e5, n=5):
+
         # simulation settings
         code_size = self.graph_settings["code_size"]
         reps = self.graph_settings["repetitions"]
@@ -141,11 +163,23 @@ class ModelTrainer:
 
         error_rates = np.linspace(min_error_rate, max_error_rate, n)
 
+        task_dict = {
+            "z": "surface_code:rotated_memory_z",
+            "x": "surface_code:rotated_memory_x",
+        }
+        code_task = task_dict[self.graph_settings["experiment"]]
+
         syndromes = []
         flips = []
         n_identities = 0
         for p in error_rates:
-            sim = SurfaceCodeSim(reps, code_size, p, int(n_graphs / n))
+            sim = SurfaceCodeSim(
+                reps,
+                code_size,
+                p,
+                int(n_graphs / n),
+                code_task=code_task,
+            )
             syndrome, flip, n_id = sim.generate_syndromes()
             syndromes.append(syndrome)
             flips.append(flip)
@@ -162,3 +196,116 @@ class ModelTrainer:
         flips = np.array_split(flips, n_splits)
 
         return syndromes, flips, n_identities
+
+    def evaluate_test_set(self, syndromes, flips, n_identities, n_graphs=5e4):
+
+        m_nearest_nodes = self.graph_settings["m_nearest_nodes"]
+        n_correct_preds = 0
+        for syndrome, flip in zip(syndromes, flips):
+
+            _n_correct_preds = inference(
+                self.model,
+                syndrome,
+                flip,
+                experiment=self.graph_settings["experiment"],
+                m_nearest_nodes=m_nearest_nodes,
+                device=self.device,
+            )
+            n_correct_preds += _n_correct_preds
+
+        val_accuracy = (n_correct_preds + n_identities) / n_graphs
+
+        return val_accuracy
+
+    def train(self):
+
+        # training settings
+        current_epoch = self.epoch
+        n_epochs = self.training_settings["epochs"]
+        dataset_size = self.training_settings["dataset_size"]
+        batch_size = self.training_settings["batch_size"]
+        n_batches = dataset_size // batch_size
+        loss_fun = self.loss_fun
+        gradient_factor = self.training_settings["gradient_factor"]
+
+        # initialise simulations and graph settings
+        m_nearest_nodes = self.graph_settings["m_nearest_nodes"]
+        n_node_features = self.graph_settings["n_node_features"]
+        power = self.graph_settings["power"]
+
+        sims = self.initialise_simulations()
+
+        # generate validation syndromes
+        n_val_graphs = self.training_settings["validation_set_size"]
+        val_syndromes, val_flips, n_val_identities = self.create_test_set(
+            n_graphs=n_val_graphs,
+        )
+
+        for epoch in range(current_epoch, n_epochs):
+            train_loss = 0
+            epoch_n_graphs = 0
+            epoch_n_trivial = 0
+            for _ in range(n_batches):
+                # simulate data as we go
+                sim = random.choice(sims)
+                syndromes, flips, n_trivial = sim.generate_syndromes()
+                epoch_n_trivial += n_trivial
+                x, edge_index, edge_attr, batch_labels, detector_labels = (
+                    get_batch_of_graphs(
+                        syndromes,
+                        m_nearest_nodes,
+                        n_node_features=n_node_features,
+                        power=power,
+                        device=self.device,
+                    )
+                )
+
+                n_graphs = syndromes.shape[0]
+
+                # forward/backward pass
+                self.optimizer.zero_grad()
+                edge_index, edge_weights, edge_classes = self.model(
+                    x, edge_index, edge_attr, detector_labels
+                )
+                loss = loss_fun(
+                    edge_index,
+                    edge_weights,
+                    edge_classes,
+                    batch_labels,
+                    flips,
+                    gradient_factor,
+                )
+                loss.backward()
+                self.optimizer.step()
+
+                train_loss += loss.item() * n_graphs
+                epoch_n_graphs += n_graphs
+
+            # compute losses and logical accuracy
+            # ------------------------------------
+
+            # train
+            train_loss /= epoch_n_graphs
+
+            # validation
+            val_accuracy = self.evaluate_test_set(
+                val_syndromes,
+                val_flips,
+                n_val_identities,
+                n_graphs=n_val_graphs,
+            )
+
+            # save training attributes after every epoch
+            self.training_history["epoch"] = epoch
+            self.training_history["train_loss"].append(train_loss)
+            self.training_history["val_accuracy"].append(val_accuracy)
+            
+            if self.save_model:
+                self.save_model_w_training_settings()
+
+    def get_training_metrics(self):
+
+        train_loss = self.training_history["train_loss"]
+        val_accuracy = self.training_history["val_accuracy"]
+
+        return train_loss, val_accuracy
