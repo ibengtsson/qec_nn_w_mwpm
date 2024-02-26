4fafa78580eebd3844be728006e3d749ca6b0009
Isak Bengtsson <isakbe@chalmers.se>
2024-02-26 17:58:42 +0100

    make model more versatile

--- src/models.py
+++ src/models.py
@@ -14,23 +14,25 @@ import sys
 def handler(signalnum, frame):
     sys.exit(0)
 
+
 def mwpm_prediction(edges, weights, classes):
     signal(SIGINT, handler)
     # convert edges to dict
     if np.unique(edges).shape[0] % 2 != 0:
+        print("Odd edges")
         print(edges)
-        print(weights)
-        print(classes)
+        # print(weights)
+        # print(classes)
     if edges.shape[1] == 0:
         print("No edges?")
         print(edges)
         print(weights)
         print(classes)
-        
-        
+
     classes = (classes > 0).astype(np.int32)
     edges_w_weights = {tuple(sorted(x)): w for x, w in zip(edges.T, weights)}
     edges_w_classes = {tuple(sorted(x)): c for x, c in zip(edges.T, classes)}
+    
     matched_edges = mwpm(edges_w_weights)
 
     # need to make sure matched_edges is sorted
@@ -59,7 +61,6 @@ class MWPMLoss(torch.autograd.Function):
     ):
 
         edge_attr = torch.stack([edge_weights, edge_classes], dim=1)
-
         # split edges and edge weights per syndrome
         (
             edges_p_graph,
@@ -140,13 +141,14 @@ class MWPMLoss(torch.autograd.Function):
 
         return None, gradients, None, None, None, None, None
 
+
 class SplitSyndromes(nn.Module):
 
     def __init__(self):
         super().__init__()
 
     def forward(self, edges, edge_attr, detector_labels):
-        
+
         node_range = torch.arange(0, detector_labels.shape[0]).to(edges.device)
         node_subset = node_range[detector_labels]
 
@@ -159,13 +161,38 @@ class SplitSyndromes(nn.Module):
 
 class GraphNN(nn.Module):
 
-    def __init__(self):
+    def __init__(
+        self,
+        hidden_channels_GCN=[32, 64, 128],
+        hidden_channels_MLP=[128, 64],
+        n_node_features=5,
+    ):
         super().__init__()
-        self.gc = nng.GraphConv(5, 16)
-        self.split_syndromes = SplitSyndromes()
 
-        self.lin1 = nn.Linear(33, 16)
-        self.lin2 = nn.Linear(16, 1)
+        # GCN layers
+        channels = [n_node_features] + hidden_channels_GCN
+        self.graph_layers = nn.ModuleList(
+            [
+                nng.GraphConv(in_channels, out_channels)
+                for (in_channels, out_channels) in zip(channels[:-1], channels[1:])
+            ]
+        )
+
+        # Dense layers
+        transition_dim = hidden_channels_GCN[-1] * 2 + 1
+        channels = [transition_dim] + hidden_channels_MLP
+        self.dense_layers = nn.ModuleList(
+            [
+                nn.Linear(in_channels, out_channels)
+                for (in_channels, out_channels) in zip(channels[:-1], channels[1:])
+            ]
+        )
+
+        # Output layer
+        self.output_layer = nn.Linear(hidden_channels_MLP[-1], 1)
+
+        # Layer to split syndrome into X (Z)-graphs
+        self.split_syndromes = SplitSyndromes()
 
     def forward(
         self,
@@ -176,36 +203,36 @@ class GraphNN(nn.Module):
         warmup=False,
     ):
 
-        x = self.gc(x, edges, edge_attr[:, 0] * edge_attr[:, 1])
-        x = torch.tanh(x)
-        
-        # split syndromes so only X (Z) nodes remain
+        w = edge_attr[:, 0] * edge_attr[:, 1]
+        for layer in self.graph_layers:
+            x = layer(x, edges, w)
+            x = torch.tanh(x)
+
+        # split syndromes so only X (Z) nodes remain and create an edge embedding
         edges, edge_attr = self.split_syndromes(edges, edge_attr, detector_labels)
-        
-        # after the graph layers, degenerate edges 0-1 and 1-0 can be removed
-        # edges, edge_attr = sort_edge_index(edges, edge_attr)
-        # edges = edges[:, :edges.shape[1] // 2]
-        # edge_attr = edge_attr[:edge_attr.shape[0] // 2, :]
-        
-        # create an edge embedding
         x_src, x_dst = x[edges[0, :]], x[edges[1, :]]
         edge_feat = torch.cat([x_src, edge_attr[:, [0]], x_dst], dim=-1)
-        edge_feat = self.lin1(edge_feat)
-        edge_feat = torch.tanh(edge_feat)
-        edge_feat = self.lin2(edge_feat)
-
+        
+        # send the edge features through linear layers
+        for layer in self.dense_layers:
+            edge_feat = layer(edge_feat)
+            edge_feat = torch.tanh(edge_feat)
+        
+        # output
+        edge_feat = self.output_layer(edge_feat)
+        
+        # if warmup, train network to do identity mapping
         if warmup:
             label = edge_attr[:, [0]]
             return edge_feat, label
-        else:
-            # save edges with largest weights
-            n_edges = edge_feat.shape[0]
-            edge_feat = edge_feat.reshape(-1, n_edges // 2)
-            edge_classes = edge_attr[:, 1].reshape(-1, n_edges // 2)
-            min_inds = torch.argmin(edge_feat, dim=0)
-            edge_feat = edge_feat[min_inds, range(n_edges // 2)]
-            edge_classes = edge_classes[min_inds, range(n_edges // 2)]
+        
+        # otherwise, save the edges with minimum weights (for each duplicate edge)
+        n_edges = edge_feat.shape[0]
+        edge_feat = edge_feat.reshape(-1, n_edges // 2)
+        edge_classes = edge_attr[:, 1].reshape(-1, n_edges // 2)
+        min_inds = torch.argmin(edge_feat, dim=0)
+        edge_feat = edge_feat[min_inds, range(n_edges // 2)]
+        edge_classes = edge_classes[min_inds, range(n_edges // 2)]
 
-            edges = edges[:, :n_edges // 2]
-            
-            return edges, edge_feat, edge_classes
+        edges = edges[:, : n_edges // 2]
+        return edges, edge_feat, edge_classes
