d34f359ced1a38b2b3b26a77dfaf7fb27dec933c
Isak Bengtsson <isakbe@chalmers.se>
2024-03-01 14:17:38 +0100

    clean up to only work on backprop branch

--- scripts/train.py
+++ scripts/train.py
@@ -37,36 +37,6 @@ def main():
     print(loss)
     print(logical_accuracy)
 
-def main_ls():
-        # command line parsing
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--configuration", required=True)
-    parser.add_argument("-s", "--save", required=False, action="store_true")
-    args = parser.parse_args() 
-    
-    # create a model
-    model = GraphNN()
-    config = Path(args.configuration)
-    
-    # check if model should be saved
-    if args.save:
-        save_or_not = args.save
-        print("Model will be saved after each epoch.")
-    else:
-        save_or_not = False
-        print("Model will not be saved.")
-    
-    # train model
-    trainer = LSTrainer(model, config=config, save_model=save_or_not)
-    trainer.train_warmup()
-    trainer.train()
-    
-    acc, logical_accuracy = trainer.get_training_metrics()
-    
-    print(acc)
-    print(logical_accuracy)
-    
-
 if __name__ == "__main__":
-    main_ls()    
+    main()
 
