2f3c29dd125236d2d6f3fa20b9febaf0afd3fedd
Frida Fjelddahl <fridafj@alvis1.int.private>
2024-03-20 10:47:04 +0100

    fixed array train

--- bash_scripts/train_array.sh
+++ bash_scripts/train_array.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 #SBATCH -A NAISS2023-5-353 -p alvis
 #SBATCH -t 3-00:00:00 			# time limit days-hours:minutes:seconds
-#SBATCH -J mwpm_nn_training
-#SBATCH -o ../job_outputs/mwpm_nn_training_%j.out
+#SBATCH -J mwpm_ls_training
+#SBATCH -o ../../alvis_out/mwpm_ls_training_%j.out
 #SBATCH --cpus-per-task=1
 #SBATCH --gpus-per-node=A40:1
 
 # purge any modules
 module purge
-apptainer exec ~/conda-cont.sif python ../scripts/train.py -c ../configs/defaults/config_${SLURM_ARRAY_TASK_ID} --save
+apptainer exec ~/conda-cont.sif python ../scripts/train.py -c ../configs/array_configs/config_${SLURM_ARRAY_TASK_ID}.yaml --save
