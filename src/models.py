d3a734b9d1f94590f88525287e7904ea213d4dce
Isak Bengtsson <isakbe@chalmers.se>
2024-04-04 13:12:25 +0200

    new variants of networks

--- src/models.py
+++ src/models.py
@@ -558,6 +558,100 @@ class SimpleGraphNNV4(nn.Module):
         super().__init__()
         
         # Weight embedding
+        self.weight_emb = nn.Linear(3, 1)
+
+        # GCN layers
+        channels = [n_node_features] + hidden_channels_GCN
+        self.graph_layers = nn.ModuleList(
+            [
+                nng.GraphConv(in_channels, out_channels)
+                for (in_channels, out_channels) in zip(channels[:-1], channels[1:])
+            ]
+        )
+    
+        # Dense layers
+        transition_dim = hidden_channels_GCN[-1] * 3 + 3
+        channels = [transition_dim] + hidden_channels_MLP
+        self.dense_layers = nn.ModuleList(
+            [
+                nn.Linear(in_channels, out_channels)
+                for (in_channels, out_channels) in zip(channels[:-1], channels[1:])
+            ]
+        )
+
+        # Output layer
+        self.output_layer = nn.Linear(hidden_channels_MLP[-1], 1)
+
+        # Layer to split syndrome into X (Z)-graphs
+        self.split_syndromes = SplitSyndromes()
+        
+        # Activation function
+        self.activation = torch.nn.ReLU()
+
+    def forward(
+        self,
+        x,
+        edges,
+        edge_attr,
+        detector_labels,
+        batch_labels,
+    ):
+
+        # weight embedding
+        w = edge_attr[:, [0]]
+        c = one_hot((edge_attr[:, 1]).to(dtype=torch.long), num_classes=2)
+        
+        w = self.weight_emb(torch.cat([w, c], dim=-1))
+        w = self.activation(w)
+        
+        # graph layers
+        for layer in self.graph_layers:
+            x = layer(x, edges, w)
+            x = self.activation(x)
+
+        # split syndromes so only X (Z) nodes remain and create an edge embedding
+        edges, edge_attr = self.split_syndromes(edges, edge_attr, detector_labels)
+        
+        # graph level information
+        w = edge_attr[:, [0]]
+        c = one_hot((edge_attr[:, 1]).to(dtype=torch.long), num_classes=2)
+        x_pool = nng.global_mean_pool(x, batch_labels)
+        inds = batch_labels[edges[0, :]]
+        x_emb = torch.cat([x_pool[inds], w, c], dim=-1)
+        
+        x_src, x_dst = x[edges[0, :]], x[edges[1, :]]
+        edge_feat = torch.cat([x_src, x_dst, x_emb], dim=-1) 
+        
+        # send the edge features through linear layers
+        for layer in self.dense_layers:
+            edge_feat = layer(edge_feat)
+            edge_feat = self.activation(edge_feat)
+        
+        # output
+        edge_feat = self.output_layer(edge_feat)
+        
+        # save the edges with minimum weights (for each duplicate edge)
+        n_edges = edge_feat.shape[0]
+        edge_feat = torch.cat([edge_feat[::2], edge_feat[1::2]], dim=1)
+        edge_classes = torch.stack([edge_attr[::2, 1], edge_attr[1::2, 1]], dim=1)
+        
+        edge_feat, min_inds = torch.min(edge_feat, dim=1)
+        edge_classes = edge_classes[range(n_edges // 2), min_inds]
+        edges = edges[:, ::2]
+        
+        return edges, edge_feat, edge_classes
+    
+class SimpleGraphNNV5(nn.Module):
+
+    def __init__(
+        self,
+        hidden_channels_GCN=[32, 64],
+        hidden_channels_MLP=[64],
+        n_node_features=5,
+    ):
+        super().__init__()
+        
+        # Weight embedding
         self.weight_emb_one = nn.Linear(3, 16)
         self.weight_emb_two = nn.Linear(16, 1)
 
@@ -624,6 +718,111 @@ class SimpleGraphNNV4(nn.Module):
         
         x_src, x_dst = x[edges[0, :]], x[edges[1, :]]
         edge_feat = torch.cat([x_src, x_dst, x_emb], dim=-1) 
+        
+        # send the edge features through linear layers
+        for layer in self.dense_layers:
+            edge_feat = layer(edge_feat)
+            edge_feat = self.activation(edge_feat)
+        
+        # output
+        edge_feat = self.output_layer(edge_feat)
+        
+        # save the edges with minimum weights (for each duplicate edge)
+        n_edges = edge_feat.shape[0]
+        edge_feat = torch.cat([edge_feat[::2], edge_feat[1::2]], dim=1)
+        edge_classes = torch.stack([edge_attr[::2, 1], edge_attr[1::2, 1]], dim=1)
+        
+        edge_feat, min_inds = torch.min(edge_feat, dim=1)
+        edge_classes = edge_classes[range(n_edges // 2), min_inds]
+        edges = edges[:, ::2]
+        
+        return edges, edge_feat, edge_classes
+    
+class SimpleGraphNNV6(nn.Module):
+
+    def __init__(
+        self,
+        hidden_channels_GCN=[32, 64],
+        hidden_channels_MLP=[64],
+        n_node_features=5,
+    ):
+        super().__init__()
+        
+        # Weight embedding
+        self.weight_emb = nn.Linear(7, 1)
+
+        # GCN layers
+        channels = [n_node_features] + hidden_channels_GCN
+        self.graph_layers = nn.ModuleList(
+            [
+                nng.GraphConv(in_channels, out_channels)
+                for (in_channels, out_channels) in zip(channels[:-1], channels[1:])
+            ]
+        )
+        
+        # Edge embedding
+        self.edge_emb = nn.Linear(3, hidden_channels_GCN[-1])
+    
+        # Dense layers
+        transition_dim = hidden_channels_GCN[-1] * 4
+        channels = [transition_dim] + hidden_channels_MLP
+        self.dense_layers = nn.ModuleList(
+            [
+                nn.Linear(in_channels, out_channels)
+                for (in_channels, out_channels) in zip(channels[:-1], channels[1:])
+            ]
+        )
+
+        # Output layer
+        self.output_layer = nn.Linear(hidden_channels_MLP[-1], 1)
+
+        # Layer to split syndrome into X (Z)-graphs
+        self.split_syndromes = SplitSyndromes()
+        
+        # Activation function
+        self.activation = torch.nn.ReLU()
+
+    def forward(
+        self,
+        x,
+        edges,
+        edge_attr,
+        detector_labels,
+        batch_labels,
+    ):
+
+        # weight embedding
+        w = edge_attr[:, [0]]
+        c = one_hot((edge_attr[:, 1]).to(dtype=torch.long), num_classes=2)
+        
+        src = x[edges[0, :], :2]
+        dst = x[edges[1, :], :2]
+        emb = torch.cat([src, w, c, dst], dim=-1)
+        
+        w = self.weight_emb(emb)
+        w = self.activation(w)
+        
+        # graph layers
+        for layer in self.graph_layers:
+            x = layer(x, edges, w)
+            x = self.activation(x)
+
+        # split syndromes so only X (Z) nodes remain and create an edge embedding
+        edges, edge_attr = self.split_syndromes(edges, edge_attr, detector_labels)
+        
+        # create an embedding for weights and classes on an edge level
+        w = edge_attr[:, [0]]
+        c = one_hot((edge_attr[:, 1]).to(dtype=torch.long), num_classes=2)
+        emb = self.edge_emb(torch.cat([w, c], dim=-1))
+        emb = self.activation(emb)
+        
+        # aggregate graph level information to aid edge weight generation
+        x_pool = nng.global_mean_pool(x, batch_labels)
+        inds = batch_labels[edges[0, :]]
+        x_pool = x_pool[inds, :]
+        
+        x_src, x_dst = x[edges[0, :]], x[edges[1, :]]
+        edge_feat = torch.cat([x_src, x_dst, x_pool, emb], dim=-1) 
         
         # send the edge features through linear layers
         for layer in self.dense_layers:
